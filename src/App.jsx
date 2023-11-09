f084289a492ed97fb7eaa8156a7c08fa6544d37c
Abdujalol <jalolhalimov203@gmail.com>
2023-11-09 22:49:41 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Footer, Login, Main, Navbar, Register } from "./components";
 const App = () => {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Main />,
+        },
+        
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
-export default App
+export default App;
